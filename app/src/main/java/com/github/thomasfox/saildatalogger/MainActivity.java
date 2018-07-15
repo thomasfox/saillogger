18601d84ce56a74b98b6ee429c66c8a4bba1b00e
Thomas Fox <fox@seitenbau.de>
2018-07-15 09:10:55 +0200

    Add menu with close action

--- app/src/main/java/com/github/thomasfox/saildatalogger/MainActivity.java
+++ app/src/main/java/com/github/thomasfox/saildatalogger/MainActivity.java
@@ -1,14 +1,15 @@
 package com.github.thomasfox.saildatalogger;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ToggleButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ToggleButton enableLoggingButton;
 
     private TextView statusText;
 
@@ -19,12 +20,28 @@ public class MainActivity extends AppCompatActivity {
 
         statusText = findViewById(R.id.statusText);
 
-        enableLoggingButton = findViewById(R.id.enableLoggingButton);
+        ToggleButton enableLoggingButton = findViewById(R.id.enableLoggingButton);
         enableLoggingButton.setOnClickListener(new EnableLoggingClickListener(statusText,this));
+
+        setSupportActionBar((Toolbar) findViewById(R.id.mainToolbar));
     }
 
-    public void enableLog(View view)
-    {
-        System.out.print("view called");
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.close:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
