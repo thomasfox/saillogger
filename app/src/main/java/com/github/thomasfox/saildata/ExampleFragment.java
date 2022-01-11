fcc71eaebf5d2f89e79fd421cc44d604fc5eff51
Thomas Fox <fischer@seitenbau.com>
2022-01-11 19:10:37 +0100

    Remove unused classes

--- app/src/main/java/com/github/thomasfox/saildata/ExampleFragment.java
+++
@@ -1,9 +0,0 @@
-package com.github.thomasfox.saildata;
-
-import androidx.fragment.app.Fragment;
-
-public class ExampleFragment extends Fragment {
-    public ExampleFragment() {
-        super(R.layout.listitem_bluetoothdevice);
-    }
-}
