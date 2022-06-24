ec89856052a3843e172752352ba2216339542c41
Thomas Fox <fischer@seitenbau.com>
2022-06-24 21:21:00 +0200

    remove commented-out code

--- app/src/main/java/com/github/thomasfox/saildata/location/LocationListenerHub.java
+++ app/src/main/java/com/github/thomasfox/saildata/location/LocationListenerHub.java
@@ -16,7 +16,6 @@ import com.github.thomasfox.saildata.sender.BleSender;
 /**
  * Receives location information from the location service and passes the information
  * to the places where location information is needed in the application.
- * Handles the android permissions necessary to access the location in the android system.
  */
 public class LocationListenerHub implements LocationListener {
 
@@ -43,7 +42,6 @@ public class LocationListenerHub implements LocationListener {
         this.screenLocationDisplayer = screenLocationDisplayer;
     }
 
-
     public void startLogging(
             @NonNull TextView locationTextView,
             int trackFileNumber,
@@ -57,22 +55,6 @@ public class LocationListenerHub implements LocationListener {
                 activity,
                 bleSender,
                 tackDirectionChangeAnalyzer);
-
-//        locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
-//        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION)
-//                != PackageManager.PERMISSION_GRANTED)
-//        {
-//            ActivityCompat.requestPermissions(activity,
-//                    new String[] {
-//                            Manifest.permission.ACCESS_FINE_LOCATION,
-//                            Manifest.permission.ACCESS_COARSE_LOCATION,
-//                            Manifest.permission.ACCESS_BACKGROUND_LOCATION},
-//                    MY_PERMISSIONS_REQUEST_LOCATION);
-//        }
-//        else
-//        {
-//            registerLocationListener();
-//        }
     }
 
     public void onLocationChanged(Location location) {
@@ -90,7 +72,6 @@ public class LocationListenerHub implements LocationListener {
 
     public void stopLogging()
     {
-//        stopLocationListener();
         compassListener.close();
         compassListener = null;
         dataLogger.close();
@@ -105,32 +86,4 @@ public class LocationListenerHub implements LocationListener {
         bleSender = null;
         tackDirectionChangeAnalyzer = null;
     }
-
-//    private void registerLocationListener()
-//    {
-//        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION)
-//                == PackageManager.PERMISSION_GRANTED)
-//        {
-//            if (USE_FAKE_LOCATION) {
-//                fakeLocationProvider = new FakeLocationProvider(this);
-//            }
-//            else {
-//                locationManager.requestLocationUpdates(
-//                        LocationManager.GPS_PROVIDER,
-//                        LOCATION_POLLING_INTERVAL_MILLIS,
-//                        LOCATION_MIN_DISTANCE_METERS,
-//                        this);
-//            }
-//            screenLocationDisplayer.displayWaitForFix();
-//        }
-//        else
-//        {
-//            screenLocationDisplayer.displayPermissionDenied();
-//        }
-//    }
-//
-//    private void stopLocationListener()
-//    {
-//        locationManager.removeUpdates(this);
-//    }
 }
